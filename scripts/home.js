896eccaf8c40f8871650c0d091944ca8efd3e7bd
Sandra <sandra@emanuel-M68MT-S2.(none)>
2014-01-08 22:30:00 +0000

    initial commit

---
+++ scripts/home.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('#sfNav').superfish();
+    externalLinks();
+    dynamicFaq();
+});
+    function externalLinks(){
+        $('a.new_window').bind('click', function(e) {
+            var location = $(this).attr('href');
+                window.open(location);
+            e.preventDefault();
+        });
+    };
+    function dynamicFaq(){
+        $('.hide').hide();
+        $('h2').bind('click', function(){
+            $(this).toggleClass('open');
+            $(this).next().slideToggle();
+        });
+    };
+
+
